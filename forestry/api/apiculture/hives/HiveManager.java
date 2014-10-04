5ab989721b4a4c139b501aa6141aa5b3d50896b2
mezz <tehgeek@gmail.com>
2014-10-04 00:43:16 -0700

    Use strings to register hives in HiveManager

--- forestry/api/apiculture/hives/HiveManager.java
+++ forestry/api/apiculture/hives/HiveManager.java
@@ -6,30 +6,46 @@
 package forestry.api.apiculture.hives;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HiveManager {
+	public static final String forest = "Forestry:forest";
+	public static final String meadows = "Forestry:meadows";
+	public static final String desert = "Forestry:desert";
+	public static final String jungle = "Forestry:jungle";
+	public static final String end = "Forestry:end";
+	public static final String snow = "Forestry:snow";
+	public static final String swamp = "Forestry:swamp";
 
-	private static ArrayList<IHive> hives = new ArrayList<IHive>();
+	private static final HashMap<String, IHive> hives = new HashMap<String, IHive>();
+
+	/**
+	 * Get a list of all hives.
+	 */
 	public static ArrayList<IHive> getHives() {
-		return new ArrayList<IHive>(hives);
+		return new ArrayList<IHive>(hives.values());
 	}
 
 	/**
 	 * Add new custom hives here to have them generate in the world.
 	 */
-	public static boolean addHive(IHive hive) {
-		return hives.add(hive);
+	public static void put(String hiveName, IHive hive) {
+		if (hives.containsKey(hiveName))
+			throw new IllegalArgumentException("Hive already exists with name: " + hiveName);
+		hives.put(hiveName, hive);
 	}
 
 	/**
 	 * Get an existing hive to add new IHiveDrops to them.
 	 */
-	public static IHive getForestHive()	{ return hives.get(0); }
-	public static IHive getMeadowsHive(){ return hives.get(1); }
-	public static IHive getDesertHive()	{ return hives.get(2); }
-	public static IHive getJungleHive()	{ return hives.get(3); }
-	public static IHive getEndHive()	{ return hives.get(4); }
-	public static IHive getSnowHive()	{ return hives.get(5); }
-	public static IHive getSwampHive()	{ return hives.get(6); }
-
+	public static IHive get(String hiveName) {
+		return hives.get(hiveName);
+	}
+	public static IHive getForestHive() { return get(forest); }
+	public static IHive getMeadowsHive() { return get(meadows); }
+	public static IHive getDesertHive() { return get(desert); }
+	public static IHive getJungleHive() { return get(jungle); }
+	public static IHive getEndHive() { return get(end); }
+	public static IHive getSnowHive() { return get(snow); }
+	public static IHive getSwampHive() { return get(swamp); }
 }
