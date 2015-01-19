1fb0beda847fe7cf3568475ca67bf36937544a9b
Samtrion <Samtrion@users.noreply.github.com>
2015-01-19 20:16:39 +0100

    Update IFlowerRegistry.java

--- forestry/api/genetics/IFlowerRegistry.java
+++ forestry/api/genetics/IFlowerRegistry.java
@@ -1,4 +1,9 @@
-package forestry.api.genetics;
+/*******************************************************************************
+ * Copyright 2011-2014 SirSengir
+ * 
+ * This work (the API) is licensed under the "MIT" License, see LICENSE.txt for details.
+ ******************************************************************************/
+ package forestry.api.genetics;
 
 import java.util.Random;
 
