af5d5fb010f7d0aa36302265b3d3d29b487201a6
mezz <tehgeek@gmail.com>
2015-04-12 17:05:43 -0700

    Deprecate redundant methods in IFlowerProvider, add getFlowerType()

--- forestry/api/genetics/IFlowerProvider.java
+++ forestry/api/genetics/IFlowerProvider.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright 2011-2014 SirSengir
- * 
+ *
  * This work (the API) is licensed under the "MIT" License, see LICENSE.txt for details.
  ******************************************************************************/
 package forestry.api.genetics;
@@ -11,17 +11,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public interface IFlowerProvider {
-	/**
-	 * @return True if the block at the passed coordinates is a valid flower for the species.
-	 */
-	boolean isAcceptedFlower(World world, IIndividual individual, int x, int y, int z);
-
-	boolean isAcceptedPollinatable(World world, IPollinatable pollinatable);
 
 	/**
-	 * @return True if a flower was planted.
+	 * @return Unique identifier for this type of flower.
 	 */
-	boolean growFlower(World world, IIndividual individual, int x, int y, int z);
+	String getFlowerType();
 
 	/**
 	 * @return Short, human-readable identifier used in the beealyzer.
@@ -29,15 +23,36 @@ public interface IFlowerProvider {
 	String getDescription();
 
 	/**
-	 * Allows the flower provider to affect the produce at the given location.
+	 * @return true if the pollinatable can be pollinated by bees of this FlowerProvider type
+	 */
+	boolean isAcceptedPollinatable(World world, IPollinatable pollinatable);
+
+	/**
+	 * Allows the flower provider to affect the bee produce at the given location.
 	 *
 	 * @return Array of itemstacks being the (modified or unmodified) produce.
 	 */
 	ItemStack[] affectProducts(World world, IIndividual individual, int x, int y, int z, ItemStack[] products);
 
 	/**
+	 * @deprecated since Forestry 3.6. Use FlowerManager.flowerRegistry.isAcceptedFlower() instead
+	 * @return True if the block at the passed coordinates is a valid flower for the species.
+	 */
+	@Deprecated
+	boolean isAcceptedFlower(World world, IIndividual individual, int x, int y, int z);
+
+	/**
+	 * @deprecated since Forestry 3.6. Use FlowerManager.flowerRegistry.growFlower() instead
+	 * @return True if a flower was planted.
+	 */
+	@Deprecated
+	boolean growFlower(World world, IIndividual individual, int x, int y, int z);
+
+	/**
+	 * @deprecated since Forestry 3.6. Use FlowerManager.flowerRegistry.getAcceptableFlowers() instead
 	 * @return List of valid flowers for the flower provider. The first in the array is for use as an icon.
 	 *  Returns an empty list if the flower provider does not have any valid flowers.
 	 */
+	@Deprecated
 	List<IFlower> getFlowers();
 }
